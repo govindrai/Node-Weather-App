352af9e0b962ce82985adcbca1ef51ae20c669de
Govind Rai <raigovind93@gmail.com>
2017-05-15 18:28:30 -0700

    Refactor code using Axios promise based library

--- app2.js
+++ app2.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const args = require('yargs')
   .options({
     a: {
@@ -9,3 +10,19 @@ const args = require('yargs')
   })
   .help()
   .argv;
+
+  let baseMapsURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+  let mapsURL = baseMapsURL + encodeURIComponent(args.address);
+
+  axios.get(mapsURL).then((response) => {
+    if (response.data.status == "ZERO_RESULTS") {
+      throw new Error("Unable to find addresss");
+    }
+    console.log(response.data);
+  }).catch((e) => {
+    if (e.code === "ENOTFOUND") {
+      console.log("Unable to connect to Google Servers :(");
+    }
+    console.log(e.message);
+  });
+
